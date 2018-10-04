20693accdd975a64706be9aa0e6a74287f4524db
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-04 11:33:11 -1000

    complete

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -7,21 +7,51 @@
  */
 function _sumFibs( maxFibValue ) {
   var sum = 0;
+ console.log(maxFibValue)
+const arr = [1, 2];
+for (let i = 0; i <arr.length; i++) {
+  if(arr[arr.length - 1] < maxFibValue) {
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  }
+}
+for (let k = 0; k < arr.length; k++) {
+  if(arr[k] % 2 === 0) {
+    sum += arr[k];
+  }
+}
 
-  // do your work here
+// var p = 0;
+// var c = 1;
+// var next;
 
+// for ( i = 1; i < 100; i++){
+//   next = c + p;
+//   p = c;
+//   c = next;
+//   if(current %2 === 0 && c < 4000000) {
+//     sum += current;
+//     console.log(sum)
+//   }
+// }
   return sum;
+  
 }
 
 // bonus round
 function _highestFibonacciNumber (maxFibValue){
-  var highest = 0;
+  var arr = [1, 2];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[arr.length - 1] < maxFibValue) {
+      arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    }
+  }
 
-  //define your base case, validate your input
-
-
-  //do your work here
-
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] < maxFibValue) {
+      highest = arr[i];
+      // return highest;
+    }
+  }
   return highest;
 };
 
--- test/even_fib_nums_spec.js
+++ test/even_fib_nums_spec.js
@@ -16,9 +16,9 @@ describe( 'Sum of even numbers in the Fibonnacci sequence', function() {
 
   // when the 2 tests pass, unskip the next test and
   // change the expected value to the correct result
-  it.skip( 'should return the sum of all even Fibonacci numbers when given 4,000,000', function() {
+  it( 'should return the sum of all even Fibonacci numbers when given 4,000,000', function() {
     var sum = fibonnaciNums.sumFibs(4000000);
-
+console.log(sum);
     // CHANGE THE VALUE OF 'X' to the correct number
     sum.should.equal( 'X' );
   });
